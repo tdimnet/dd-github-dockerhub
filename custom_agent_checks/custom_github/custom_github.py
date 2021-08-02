eda9e92a4b150b78802680564fb3aa062446f732
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-08-02 16:14:31 +0200

    Also add the number of PR

--- custom_agent_checks/custom_github/custom_github.py
+++ custom_agent_checks/custom_github/custom_github.py
@@ -4,6 +4,12 @@ from datadog_checks.base import AgentCheck
 
 
 class GithubRepoCheck(AgentCheck):
+    def get_pull_requests_list(self):
+        r = requests.get(f'https://api.github.com/repos/tdimnet/front-dd/pulls')
+
+        if r.status_code == 200:
+            return len(r.json())
+
     def get_issues_list(self):
 
         r = requests.get(f'https://api.github.com/repos/tdimnet/front-dd/issues')
@@ -13,6 +19,8 @@ class GithubRepoCheck(AgentCheck):
 
     def check(self, instance):
         number_of_issues = self.get_issues_list()
+        number_of_pull_requests = self.get_pull_requests_list()
 
         self.gauge('github.numberOfIssues', number_of_issues, tags=['TAG_KEY:TAG_VALUE'] + self.instance.get('tags', []))
+        self.gauge('github.numberOfPR', number_of_pull_requests, tags=['TAG_KEY:TAG_VALUE'] + self.instance.get('tags', []))
 
