5b266623259fe9f4107ba624f89a9c8f154ae801
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-08-02 16:51:03 +0200

    Add access token

--- custom_agent_checks/custom_github/custom_github.py
+++ custom_agent_checks/custom_github/custom_github.py
@@ -1,18 +1,21 @@
 import requests
+import os
 
 from datadog_checks.base import AgentCheck
 
 
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+
+
 class GithubRepoCheck(AgentCheck):
     def get_pull_requests_list(self):
-        r = requests.get(f'https://api.github.com/repos/tdimnet/front-dd/pulls')
+        r = requests.get(f'https://api.github.com/repos/tdimnet/front-dd/pulls?access_token={ACCESS_TOKEN}')
 
         if r.status_code == 200:
             return len(r.json())
 
     def get_issues_list(self):
-
-        r = requests.get(f'https://api.github.com/repos/tdimnet/front-dd/issues')
+        r = requests.get(f'https://api.github.com/repos/tdimnet/front-dd/issues?access_token={ACCESS_TOKEN}')
 
         if r.status_code == 200:
             return len(r.json())
@@ -23,4 +26,3 @@ class GithubRepoCheck(AgentCheck):
 
         self.gauge('github.numberOfIssues', number_of_issues, tags=['TAG_KEY:TAG_VALUE'] + self.instance.get('tags', []))
         self.gauge('github.numberOfPR', number_of_pull_requests, tags=['TAG_KEY:TAG_VALUE'] + self.instance.get('tags', []))
-
